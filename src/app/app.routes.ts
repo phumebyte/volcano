4ce36afb46b9ec14e82e6a1dbba747212de23ae2
Phumelele Dlamini <phumelele.dlamini1000@gmail.com>
2026-02-16 01:03:42 +0200

    fixed minor things

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { SingleComponent } from './pages/single/single.component';
 import { SplashComponent } from './pages/splash/splash.component';
 import { LandingComponent } from './pages/landing/landing.component';
-import { ConfettiComponent } from './components/confetti/confetti.component';
 import { SweetValentineComponent } from './components/sweet-valentine/sweet-valentine.component';
 import { LoveCalculatorComponent } from './components/love-calculator/love-calculator.component';
 import { LoadingComponent } from './components/loading/loading.component';
@@ -10,13 +9,14 @@ import { ResultsComponent } from './components/results/results.component';
 import { MessageComponent } from './pages/message/message.component';
 
 export const routes: Routes = [
-    { path: 'splash', component: SplashComponent },
-    { path: 'landing', component: LandingComponent },
-    { path: 'single', component: SingleComponent },
-    { path: 'confetti', component: ConfettiComponent },
-    { path: 'sweet-valentine', component: SweetValentineComponent}, 
-    { path: 'love-calculator', component: LoveCalculatorComponent},
-    { path: 'loading', component: LoadingComponent}, 
-    { path: 'results', component: ResultsComponent}, 
-    { path: 'message', component: MessageComponent},
+  { path: '', redirectTo: 'splash', pathMatch: 'full' }, // default route
+  { path: 'splash', component: SplashComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: 'single', component: SingleComponent },
+  { path: 'sweet-valentine', component: SweetValentineComponent }, 
+  { path: 'love-calculator', component: LoveCalculatorComponent },
+  { path: 'loading', component: LoadingComponent }, 
+  { path: 'results', component: ResultsComponent }, 
+  { path: 'message', component: MessageComponent },
+  { path: '**', redirectTo: 'splash' } // wildcard fallback
 ];
